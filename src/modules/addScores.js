0867cac26dfda70e33f0a0bd97b7c3c47a55a1ce
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2022-10-21 22:28:36 +0200

    Update api

---
+++ src/modules/addScores.js
@@ -0,0 +1,42 @@
+import API_url from "./APIconfig.js";
+
+const playerName = document.querySelector('#player-name');
+const playerScore = document.querySelector('#player-score');
+const form = document.querySelector('form');
+const messagePopup = document.querySelector('#add-msg')
+
+const addScore = async () => {
+    const user = playerName.value;
+    const score = playerScore.value;
+    const scoreOBJ = { user, score };
+    if (!user || !score) {
+        messagePopup.innerHTML = 'Please add a player name and score';
+        messagePopup.style.color = "red";
+      return;
+    }
+
+    try {
+      const res = await fetch(API_url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: JSON.stringify(scoreOBJ),
+      });
+
+      const data = await res.json();
+      if (!res.ok) {
+        messagePopup.innerHTML = 'Your score could not be added, please try again';
+        messagePopup.style.color = "red";
+      }
+      messagePopup.innerHTML = data.result;
+      form.reset();
+    } catch (err) {
+        messagePopup.innerHTML = 'Your score could not be added, please try again';
+    }
+
+    setTimeout(() => {
+        messagePopup.innerHTML = '';
+    }, 2000);
+  };
+  export default addScore;
