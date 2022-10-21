d79f099cc4cb2d7345a0bebed3341f30681b1f30
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2022-10-21 22:28:44 +0200

    Create scoresList.js

---
+++ src/modules/scoresList.js
@@ -0,0 +1,26 @@
+import API_url from './APIconfig.js';
+
+const scoreList = document.querySelector('.list-scores');
+const messagePopup = document.querySelector('#add-msg');
+const ListScores = async () => {
+  let alerts = '';
+
+  try {
+    const res = await fetch(API_url);
+    const data = await res.json();
+    if (!res.ok) {
+        scoreList.innerHTML = 'Update';
+    }
+    data.result.sort((a, b) => b.score - a.score).forEach((item) => {
+        alerts += `<tr><td>${item.user}: <span>${item.score}</span></td></tr>`;
+    });
+    scoreList.innerHTML = alerts;
+    messagePopup.innerHTML = 'Your scores have been updated successfully';
+    setTimeout(() => {
+        messagePopup.innerHTML = '';
+    }, 2000);
+  } catch (err) {
+    scoreList.innerHTML = 'Down Time';
+  }
+};
+export default ListScores;
