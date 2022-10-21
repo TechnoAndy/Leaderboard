2f2365eaa5c7a83f0f63cdc892a9a756728fe6b3
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2022-10-21 22:28:24 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './style.css';
+import addScores from './modules/addScores.js';
+import scoresList from './modules/scoresList';
+
+const refreshBtn = document.querySelector('#update');
+const submitBtn = document.querySelector('#new-score');
+
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    addScores();
+  });
+
+refreshBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    scoresList();
+    console.log("man")
+  });
+
+
+
+
+
+
